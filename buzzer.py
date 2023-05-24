9f84d89e46bcc93dac9387f597efee21e47b9b7b
Christian Ducros <96232175+christianDUCROS@users.noreply.github.com>
2023-05-24 13:57:26 +0200

    buzzer.py

--- buzzer.py
+++ buzzer.py
@@ -8,9 +8,10 @@ def play_son(freq,duty,duree) :
     pwm0.duty_u16(0)
     time.sleep_ms(50) #valeur
 
-print('son 1')
-play_son(440,32767,1000)
-time.sleep(1)
-print('son 2')
-play_son(440,5000,1000)
-time.sleep(1)
+if __name__ == "__main__":    
+    print('son 1')
+    play_son(440,32767,1000)
+    time.sleep(1)
+    print('son 2')
+    play_son(440,5000,1000)
+    time.sleep(1)
